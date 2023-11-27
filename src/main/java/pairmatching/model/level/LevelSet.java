bc0409b54dda30e80fd78ee727d9ae6d4429e3da
JAY-Winter <rubat0@naver.com>
2023-11-27 16:27:12 +0900

    feat: Level

--- src/main/java/pairmatching/model/level/LevelSet.java
+++ src/main/java/pairmatching/model/level/LevelSet.java
@@ -1,2 +1,20 @@
-package pairmatching.model.level;public enum LevelSet {
+package pairmatching.model.level;
+
+public enum LevelSet {
+    LEVEL1("레벨1"),
+    LEVEL2("레벨2"),
+    LEVEL3("레벨3"),
+    LEVEL4("레벨4"),
+    LEVEL5("레벨5"),
+    ;
+
+    public String getName() {
+        return name;
+    }
+
+    private final String name;
+
+    LevelSet(String name) {
+        this.name = name;
+    }
 }
