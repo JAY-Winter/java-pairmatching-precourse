e958492c902ae017da6976a337a2b10385548cbc
JAY-Winter <rubat0@naver.com>
2023-11-27 16:26:47 +0900

    feat: View

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,12 +1,13 @@
 package pairmatching.view;
 
-import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 import java.util.Scanner;
 import pairmatching.model.feature.Feature;
 import pairmatching.model.feature.FeatureSet;
+import pairmatching.model.program.LevelMissions;
+import pairmatching.model.program.Program;
 
 public class InputView {
-
 
     public Feature requestSelectFunction() {
         System.out.println("기능을 선택하세요.");
@@ -25,5 +26,20 @@ public class InputView {
             return requestSelectFunction();
         }
         return feature;
+    }
+
+    public Program requestProgramInformation(List<LevelMissions> initLevelMissions) {
+        Program program;
+        try {
+            System.out.println("과정, 레벨, 미션을 선택하세요.");
+            System.out.println("ex) 백엔드, 레벨1, 자동차경주");
+            String input = new Scanner(System.in).nextLine();
+
+            program = Program.from(input, initLevelMissions);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return requestProgramInformation(initLevelMissions);
+        }
+        return program;
     }
 }
